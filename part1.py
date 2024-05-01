e8c80f537e58c88defa3a4c1af226923e528b5e4
fannybdt <132360390+fannybdt@users.noreply.github.com>
2024-05-01 17:25:04 +0200

    Part 1a) finished

--- part1.py
+++ part1.py
@@ -12,7 +12,6 @@ Control_data = scipy.io.loadmat(data_path + "Control.mat")
 
 ARDS_data = {key: ARDS_data[key][0] for key in ["P", "Q", "V", "t"]}
 Control_data = {key: Control_data[key][0] for key in ["P", "Q", "V", "t"]}
-
 def extract_cycles(data):
     cycles = []
 
@@ -30,27 +29,36 @@ def extract_cycles(data):
 
     filtered_V_indices.append(zero_V_indices[0])
 
-    V_sequence = [zero_V_indices[0]]
-
     for i in range(1, len(zero_V_indices)):
-        
         if zero_V_indices[i] != zero_V_indices[i-1] + 1:
-            filtered_V_indices.append(V_sequence[len(V_sequence)//2])
-            V_sequence = []
+            filtered_V_indices.append(zero_V_indices[i])
 
-        V_sequence.append(zero_V_indices[i])
-    
-    filtered_V_indices.append(V_sequence[len(V_sequence)//2])
-        
     for i in range(0, len(filtered_V_indices) - 1):
-        cycles.append({'t' : t[filtered_V_indices[i]:filtered_V_indices[i+1]+1] - t[filtered_V_indices[i]],
+        cycles.append({'t' : t[filtered_V_indices[i]:filtered_V_indices[i+1]+1],
                        'P' : P[filtered_V_indices[i]:filtered_V_indices[i+1]+1],
                        'Q' : Q[filtered_V_indices[i]:filtered_V_indices[i+1]+1],
                        'V' : V[filtered_V_indices[i]:filtered_V_indices[i+1]+1]})
+    """
+    plt.figure()
+    plt.plot(t, V)
+    for k in range(len(filtered_V_indices)):
+        plt.axvline(t[filtered_V_indices[k]], color='red')
+    plt.show()
 
-    return cycles 
+    plt.figure()
+    plt.plot(t, Q)
+    for k in range(len(filtered_V_indices)):
+        plt.axvline(t[filtered_V_indices[k]], color='red')
+    plt.show()
 
-def get_works(data, title="Data",alpha_value = 0.5, x_label = "X", y_label ="Y", grid = True):
+    plt.figure()
+    plt.plot(t, P)
+    for k in range(len(filtered_V_indices)):
+        plt.axvline(t[filtered_V_indices[k]], color='red')
+    plt.show()
+    """
+    return cycles
+def plot_time_curves(data, name = "salut", title="Data",alpha_value = 0.5, x_label = "X", y_label ="Y", grid = True):
     P = data['P']
     V = data['V']
     Q = data['Q']
@@ -79,31 +87,15 @@ def get_works(data, title="Data",alpha_value = 0.5, x_label = "X", y_label ="Y",
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.title(title)
-
     minP = np.argmin(P)
-    minV = 0
-    index_inspi = np.where(Q >= 0)
-    index_inspi = index_inspi[0]
+    index_inspi = np.where(Q >= 0)[0]
     V_Q_0 = V[index_inspi[-1]]
     P_Q_0 = P[index_inspi[-1]]
-
-    P_inspi = P[index_inspi]
-    V_inspi = V[index_inspi]
-
-    a,b = np.polyfit([P[minP], P_Q_0],  [0, V_Q_0], deg = 1)
-    px = V_inspi/a - b
-    vy = P_inspi*a +b
-
-    equation_label = f"y = {a:.3f}x + {b:.3f}"
-    ax.plot([P[minP], P_Q_0],  [0, V_Q_0], label = equation_label, color ="deepskyblue")
+    ax.plot([P[minP], P_Q_0],  [0, V_Q_0], label = 'Inspiration/Expiration Separation', color ="deepskyblue")
+    ax.plot([P[minP], P[minP]], [0, V_Q_0], label = 'Limit of Elastic Work', color = "midnightblue", linestyle = 'dashed')
+    ax.plot([P[minP], P_Q_0], [V_Q_0, V_Q_0], color = "midnightblue", linestyle = 'dashed')
     ax.legend(loc = 'lower right')
-
-    RIW = np.trapz(P_inspi-px, V_inspi)
-    REW = np.trapz(P,V) - RIW
-    plt.plot(P_inspi, V_inspi,color = "mediumpurple")
-    plt.fill_between(P,V, color = "lavender")
-    #plt.fill_between(P_inspi, V_inspi, color = "mediumpurple", label = 'RIW')
-    plt.legend(loc = 'lower right')
+    plt.savefig(name, dpi = 150)
     plt.show()
     plt.figure(2)
     print("---VQ loop---")
@@ -132,9 +124,48 @@ def get_works(data, title="Data",alpha_value = 0.5, x_label = "X", y_label ="Y",
     plt.show()
     
 
-    return RIW, REW
+def get_RIW_REW_EW(data):
+    P = data['P']
+    V = data['V']
+    Q = data['Q']
+    t = data['t'] - data['t'][0]
+
+    minP = np.argmin(P)
+    index_inspi = np.where(Q >= 0)[0]
+
+    V_Q_0 = V[index_inspi[-1]]
+    P_Q_0 = P[index_inspi[-1]]
+    base = np.abs(P_Q_0 - P[minP])
+    print(base)
+    
+    EW = (base * V_Q_0)/2
+
+    P_expi = P[index_inspi[-1]:]
+    V_expi = V[index_inspi[-1]:]   
+
+    a,b = np.polyfit([P[minP], P_Q_0],  [0, V_Q_0], deg = 1)
+    y = P_expi*a +b
+    V_expi_adjusted = y-V_expi
+    REW = np.abs(np.trapz(V_expi_adjusted, P_expi))
+  
+    RIW = np.abs(np.trapz(V, P)) -(REW)
+    return RIW, REW, EW
+
+
 
 cycles = extract_cycles(Control_data)
+plot_time_curves(cycles[3], name = "drawwork.png", title = " ", x_label= "Pressure [cmH2O]", y_label= "Volume [l]")
+RIW = []
+REW = []
+EW = []
 
-RIW, REW = get_works(cycles[3])
-print(RIW, REW)
+"""
+for i in range(len(cycles)):
+    plot_time_curves(cycles[i])
+    riw, rew, ew = get_RIW_REW_EW(cycles[i])
+    RIW.append(riw)
+    REW.append(rew)
+    EW.append(ew)
+print(len(cycles))
+print(np.std(RIW), np.std(REW), np.std(EW))
+""" 
